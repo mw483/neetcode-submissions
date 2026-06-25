72ac96c3ad87e37181a63ff35d3cc5171cc6cc3a
mw483 <159742840+mw483@users.noreply.github.com>
2026-06-25 14:59:34 +0900

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Use deque
+        from collections import deque
+        output = []
+
+        # initialize empty window as a deque
+        window = deque()
+
+        # loop through array
+        l = 0
+        # use fixed size sliding window of size k
+        for r in range(len(nums)):
+            # append to the fixed size sliding window when the 
+            # window is still smaller than requested
+            window.append(nums[r])
+
+            # start calculating the max and append-remove, 
+            # and slide the left pointer
+            # only if the window if the valid size
+            if r - l >= k - 1:
+                output.append(max(window))
+                window.popleft()
+                l += 1
+
+        return output
