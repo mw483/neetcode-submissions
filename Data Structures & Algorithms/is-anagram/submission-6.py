4bf213a2aed67c52446a42107ee0c94a8317de97
mw483 <159742840+mw483@users.noreply.github.com>
2026-07-25 20:51:11 +0900

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = [0] * 26
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+        if not any(freq):
+            return True
+        else:
+            return False
